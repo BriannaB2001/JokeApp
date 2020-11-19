5c5857fd0841a757913f6f8d6ac6a50e2d920495
Brianna <nanitooze@gmail.com>
2020-11-19 10:09:31 -0700

    NSFetchResultControllerStarted

--- JokeApp/UI/FavoritesTableViewController.swift
+++ JokeApp/UI/FavoritesTableViewController.swift
@@ -10,15 +10,41 @@ import CoreData
 
 class FavoritesTableViewController: UITableViewController {
     
+    fileprivate lazy var fetchedResultsController: NSFetchedResultsController<Entry> = {
+        // Create Fetch Request
+        let fetchRequest: NSFetchRequest<Entry> = Entry.fetchRequest()
+        // Configure Fetch Request
+        fetchRequest.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: false)]
+        // Create Fetched Results Controller
+        let fetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: CoreDataStack.shared.context, sectionNameKeyPath: nil, cacheName: nil)
+        // Configure Fetched Results Controller
+        fetchedResultsController.delegate = self
+        return fetchedResultsController
+    }()
+    
+    let context = CoreDataStack.shared.context
+   lazy var fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Entry")
+    lazy var controller = {
+        NSFetchedResultsController<NSFetchRequestResult>(fetchRequest: fetchRequest, managedObjectContext: CoreDataStack.shared.context, sectionNameKeyPath: nil, cacheName: nil)
+        
+        
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        do {
+        try controller.performFetch()
+        } catch {
+            fatalError("Failed to fetch entities: \(error)")
+        }
     }
 
     // MARK: - Table view data source
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 1
+        return fetchedResultsController.fetchedObjects?.count ?? 0
     }
 
     
@@ -31,4 +57,8 @@ class FavoritesTableViewController: UITableViewController {
     }
     
 
+}
+
+extension FavoritesTableViewController: NSFetchedResultsControllerDelegate {
+    
 }
