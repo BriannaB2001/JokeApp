e6cfb6bce8b8a69c7afc8522f6e050cef6e1b52a
Talmage <talmagelynn@gmail.com>
2020-12-22 09:29:25 -0700

    add save for daddy
    oop

--- JokeApp/UI/DadJokes/DadJokesViewController.swift
+++ JokeApp/UI/DadJokes/DadJokesViewController.swift
@@ -41,6 +41,23 @@ class DadJokesViewController: UIViewController {
             self.dadJokeItem = dadJokeItem
             self.displayResult()
         }
+        
+    }
+    
+    @IBOutlet weak var saveButton: UIButton!
+    
+    @IBAction func saveButtonPressed(_ sender: Any) {
+        guard let dadJokeItem = dadJokeItem else { return }
+        
+        if let existingEntry =  CoreDataManager.shared.entryForText(text: dadJokeItem ) {
+            CoreDataManager.shared.deleteEntry(entry: existingEntry)
+            saveButton.isSelected = false
+        } else {
+            CoreDataManager.shared.createNewEntry(text: dadJokeItem, type: .dadJoke)
+            saveButton.isSelected = true
+        }
+        
+        
     }
     
     
